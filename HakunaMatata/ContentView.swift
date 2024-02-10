64c2fd20e384eef328ac0d463bb8af6033d81541
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2024-02-10 21:12:09 +0100

    initial commit

---
+++ HakunaMatata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HakunaMatata
+//
+//  Created by Djordje Arandjelovic on 10.2.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HakunaMatata/HakunaMatataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HakunaMatataApp.swift
+//  HakunaMatata
+//
+//  Created by Djordje Arandjelovic on 10.2.24..
+//
+
+import SwiftUI
+
+@main
+struct HakunaMatataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
